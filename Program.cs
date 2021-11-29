2f894245dbbd396b0c8107fc5fed32192ecee819
Buksha Kirill <buksha1024@gmail.com>
2021-11-29 23:48:00 +0300

    Implemented hello-world example

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using File = System.IO.File;
 
 namespace TelegramBotAvailabilityCheck
 {
@@ -11,6 +16,44 @@ namespace TelegramBotAvailabilityCheck
         {
             var secretToken = ReadSecretToken();
             var bot = CreateBot(secretToken);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+
+            ConfigureBot(bot, cancellationToken);
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureBot(TelegramBotClient bot, CancellationToken cancellationToken)
+        {
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } // receive all update types
+            };
+
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+        }
+
+        static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is { } message)
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Hello", cancellationToken: cancellationToken);
+            }
+        }
+
+        static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is ApiRequestException apiRequestException)
+            {
+                //await botClient.SendTextMessageAsync(adminId, apiRequestException.ToString(), cancellationToken: cancellationToken);
+            }
         }
 
         private static TelegramBotClient CreateBot(string secretToken)
